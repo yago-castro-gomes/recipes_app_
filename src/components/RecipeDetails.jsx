0674e224885b2e51b4559c14eb916558567a4105
Rafael Rodrigues Bizachi <rafbizachi@gmail.com>
2023-01-30 17:53:08 -0300

    feat: implementação da pagina principal de receitas

--- src/components/RecipeDetails.jsx
+++ src/components/RecipeDetails.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 export default function RecipeDetails() {
+  const { id } = useParams();
   return (
-    <div>RecipeDetails</div>
+    <div>{id}</div>
   );
 }
